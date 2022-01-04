dda5069274344dc45872da75d5267eb4063139d2
Serge Demeyer <serge.demeyer@uantwerpen.be>
2022-01-04 15:53:38 +0100

    Added version of findLast and updated main

---
+++ find.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+
+int findLast(std::vector<int> x, int y) {
+    if (x.size() == 0)
+        return -1;
+    for (int i = x.size() - 1; i >= 0; i--)
+        if (x[i] == y)
+            return i;
+    return -1;
+}
+
+
+
+
+
+
+
+
+
+//Mutant 1 - replace i >= 0 by i < 0
+//All tests which return an actual value will fail
+//[  FAILED  ] FindLastTests.doubleOccurrence (0 ms)
+
+//Mutant 2 - replace i >= 0 by i >0
+//for (int i = x.size() - 1; i >= 0; i--)
+//for (int i = x.size() - 1; i > 0; i--)
+//This mutant is not killed, we need to add an extra test
+
+//Mutant 3 - replace (x[i] == y) by (x[i] != y)
+//All tests with a non-empty vector will fail
+//[  FAILED  ] FindLastTests.doubleOccurrence (0 ms)
+//[  FAILED  ] FindLastTests.noOccurrence (1 ms)
+//Mutant 2 is subsumed by mutant 1 -- all tests that kill mutant 2 also kill mutant 1.
+//Mutant 1 is harder to kill
+
+
+//Mutant 4 - replace == 0 by <=0
+//if (x.size() <= 0) return -1;
+//if (x.size() == 0) return -1;
+//This is an equivalent mutant
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <vector>
+#include "find.cpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::vector<int> v{1, 2, 42, 42, 63};
+    std::cout << findLast(v, 99) << " == -1 (not present)." << '\n';
+    std::cout << findLast(v, 63) << " == 4 (terminating position)" << '\n';
+    std::cout << findLast(v, 42) << " == 3 (one but last position)." << '\n';
+    std::cout << findLast(v, 2) << " == 1 (2nd position)." << '\n';
+    std::cout << findLast(v, 1) << " == 0 (very first position)." << '\n';
+    std::cout << findLast(v, 0) << " == -1 (not present)." << '\n';
+    std::vector<int> v2{};
+    std::cout << findLast(v2, 99) << " == -1 (empty vector)." << '\n';
 }
