288ee7734488b5803d98a682fa5bf161642ae268
Serge Demeyer <serge.demeyer@uantwerpen.be>
2022-01-05 22:17:20 +0100

    Prepared all mutants

--- find.cpp
+++ find.cpp
@@ -11,6 +11,32 @@ int findLastOccurrence(std::string toSearch, char toFind) {
 }
 
 
+//Correct code for the for loop
+//for (int i = toSearch.size() - 1; i >= 0; i--)
+
+
+//Mutant 1 - replace i >= 0 by i < 0
+//All tests which return an actual value will fail
+//[  FAILED  ] FindLastTests.doubleOccurrence (0 ms)
+
+//Mutant 2 - replace i >= 0 by i >0
+//for (int i = x.size() - 1; i >= 0; i--)
+//for (int i = x.size() - 1; i > 0; i--)
+//This mutant is not killed, we need to add an extra test
+
+//Mutant 3 - replace (toSearch[i] == toFind) by (toSearch[i] != toFind)
+//All tests with a non-empty vector will fail
+//[  FAILED  ] FindLastTests.doubleOccurrence (0 ms)
+//[  FAILED  ] FindLastTests.noOccurrence (1 ms)
+//Mutant 3 is subsumed by mutant 1 -- all tests that kill mutant 3 also kill mutant 1.
+//Mutant 1 is harder to kill
+
+//Mutant 4 - replace == 0 by <=0
+//if (toSearch.size() <= 0)
+//This is an equivalent mutant
+
+
+
 int findLast(std::vector<int> x, int y) {
     if (x.size() == 0)
         return -1;
@@ -36,7 +62,6 @@ int findLast(std::vector<int> x, int y) {
 //[  FAILED  ] FindLastTests.noOccurrence (1 ms)
 //Mutant 3 is subsumed by mutant 1 -- all tests that kill mutant 3 also kill mutant 1.
 //Mutant 1 is harder to kill
-
 
 //Mutant 4 - replace == 0 by <=0
 //if (x.size() <= 0) return -1;
--- tests.cpp
+++ tests.cpp
@@ -16,58 +16,58 @@ TEST(FindLastOccurrenceTests, emptyString) {
 }
 
 TEST(FindLastOccurrenceTests, occurrenceOnBoundary) {
-    EXPECT_EQ(0, findLastOccurrence("Hello Worls", 'H'));
+    EXPECT_EQ(0, findLastOccurrence("Hello World", 'H'));
 }
 
 
-
-TEST(FindLastTests, noOccurrence) {
-    EXPECT_EQ(-1, findLast({1, 2, 42, 42, 63}, 99));
-}
-
-TEST(FindLastTests, doubleOccurrence) {
-    EXPECT_EQ(3, findLast({1, 2, 42, 42, 63}, 42));
-}
-
-TEST(FindLastTests, emptyVector) {
-    EXPECT_EQ(-1, findLast({}, 3));
-}
-
-TEST(FindLastTests, occurrenceOnBoundary) {
-    EXPECT_EQ(0, findLast({1, 2, 42, 42, 63}, 1));
-}
-
-/*TEST(FindLastTests, wrongAssert) {
-    EXPECT_EQ(-1, findLast({1, 2, 42, 42, 63}, 1));
-    // This reaches the fault, infects the program state and propagates but the assert statement is wrong
-}*/
-
-
-/*TEST(FindLastTests, emptyVectorPostConditions) {
-    int res1, res2;
-    EXPECT_EQ(-1, res1 = findLast({}, 3));
-    EXPECT_EQ(-1, res2 = findLast({}, 3));
-    //@ ensures res1 == res2;
-}*/
-
-
-
-TEST(FindFirstTests, noOccurrence) {
-    EXPECT_EQ(-1, findFirst({1, 2, 42, 42, 63}, 99));
-}
-
-TEST(FindFirstTests, doubleOccurrence) {
-    EXPECT_EQ(2, findFirst({1, 2, 42, 42, 63}, 42));
-}
-
-TEST(FindFirstTests, emptyVector) {
-    EXPECT_EQ(-1, findFirst({}, 3));
-}
-
-TEST(FindFirstTests, occurrenceOnBoundary) {
-    EXPECT_EQ(4, findFirst({1, 2, 42, 42, 63}, 63));
-}
-
+//
+//TEST(FindLastTests, noOccurrence) {
+//    EXPECT_EQ(-1, findLast({1, 2, 42, 42, 63}, 99));
+//}
+//
+//TEST(FindLastTests, doubleOccurrence) {
+//    EXPECT_EQ(3, findLast({1, 2, 42, 42, 63}, 42));
+//}
+//
+//TEST(FindLastTests, emptyVector) {
+//    EXPECT_EQ(-1, findLast({}, 3));
+//}
+//
+//TEST(FindLastTests, occurrenceOnBoundary) {
+//    EXPECT_EQ(0, findLast({1, 2, 42, 42, 63}, 1));
+//}
+//
+///*TEST(FindLastTests, wrongAssert) {
+//    EXPECT_EQ(-1, findLast({1, 2, 42, 42, 63}, 1));
+//    // This reaches the fault, infects the program state and propagates but the assert statement is wrong
+//}*/
+//
+//
+///*TEST(FindLastTests, emptyVectorPostConditions) {
+//    int res1, res2;
+//    EXPECT_EQ(-1, res1 = findLast({}, 3));
+//    EXPECT_EQ(-1, res2 = findLast({}, 3));
+//    //@ ensures res1 == res2;
+//}*/
+//
+//
+//
+//TEST(FindFirstTests, noOccurrence) {
+//    EXPECT_EQ(-1, findFirst({1, 2, 42, 42, 63}, 99));
+//}
+//
+//TEST(FindFirstTests, doubleOccurrence) {
+//    EXPECT_EQ(2, findFirst({1, 2, 42, 42, 63}, 42));
+//}
+//
+//TEST(FindFirstTests, emptyVector) {
+//    EXPECT_EQ(-1, findFirst({}, 3));
+//}
+//
+//TEST(FindFirstTests, occurrenceOnBoundary) {
+//    EXPECT_EQ(4, findFirst({1, 2, 42, 42, 63}, 63));
+//}
+//
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
